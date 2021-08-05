85fdb4fbecb000ddf877ec7d8a5c5e66ee1d9f3b
devnivek <kevin.santos@outlook.com>
2021-08-04 22:10:05 -0300

    Atividade Spring Farmacia

---
+++ spring/farmacia/src/main/java/com/farmacia/model/Produto.java
@@ -0,0 +1,80 @@
+package com.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank
+	@Size(min = 5, max = 100)
+	private String nome;
+	
+	@NotBlank
+	@Min(1)
+	private double preco;
+	
+	@NotBlank
+	@Min(0)
+	private int quantidade;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria; 
+	
+
+}
